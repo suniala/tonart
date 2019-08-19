a8783eda730844e6ef5a3eab46a7a9aa63a6edd2
Mikko Suniala <mikko.suniala@iki.fi>
2019-08-19 09:59:47 +0300

    Try out route params

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {BrowserRouter as Router, Link, Route} from 'react-router-dom';
+import {RouteComponentProps} from 'react-router';
 import Moro from "./Moi";
 import Hello from "./Hello";
 
@@ -14,14 +15,14 @@ const App: React.FC = () => {
                                 <Link to="/">Moro</Link>
                             </li>
                             <li>
-                                <Link to="/hello/">Hello</Link>
+                                <Link to="/hello/Teppo">Hello</Link>
                             </li>
                         </ul>
                     </nav>
 
-
                     <Route path="/" exact component={Moro}/>
-                    <Route path="/hello/" render={() => <Hello name={'Teppo'}/>}/>
+                    <Route path="/hello/:name" render={(props: RouteComponentProps<{ name: string }>) =>
+                        <Hello name={props.match.params.name}/>}/>
                 </div>
             </Router>
         </div>
