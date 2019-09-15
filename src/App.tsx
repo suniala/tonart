45ea78134b1d43a852b3be4be6dab347f4288272
Mikko Suniala <mikko.suniala@iki.fi>
2019-09-15 17:03:40 +0300

    Remove react-router-dom, update package-lock

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
 import React from 'react';
-import {HashRouter, Route} from 'react-router-dom';
 import Tahti from "./Tahti";
 
 const App: React.FC = () => {
     return (
-        <HashRouter>
-            <div className="container">
-                <div className="row">
-                    <Route path="/" exact component={Tahti}/>
-                </div>
+        <div className="container">
+            <div className="row">
+                <Tahti/>
             </div>
-        </HashRouter>
+        </div>
     );
 };
 
