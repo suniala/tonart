f80f001641cb5c07455c235350c537a496712498
Mikko Suniala <mikko.suniala@iki.fi>
2019-10-19 16:52:09 +0300

    Show/hide key signature keyboard on click

--- src/KeySignaturePanel.tsx
+++ src/KeySignaturePanel.tsx
@@ -6,14 +6,31 @@ interface Props {
     keySignature: KeySignature
 }
 
-class KeySignaturePanel extends React.Component<Props, {}> {
+interface State {
+    opened: boolean
+}
+
+class KeySignaturePanel extends React.Component<Props, State> {
+    public constructor(props: Props) {
+        super(props);
+        this.state = {
+            opened: false
+        };
+    }
+
+    private toggle = () => {
+        this.setState((prev) => prev.opened ? {opened: false} : {opened: true})
+    };
+
     render() {
         return (
-            <div key={this.props.keySignature.name}>
-                <div>
+            <div className='key-signature-panel'
+                 key={this.props.keySignature.name}
+                 onClick={this.toggle}>
+                <div className='title'>
                     {this.props.keySignature.name}
                 </div>
-                <div>
+                <div className={'tones' + (this.state.opened ? ' open' : ' closed')}>
                     <Keyboard tones={new Set(this.props.keySignature.tones)} readOnly={true}/>
                 </div>
             </div>
